b724b3317bc249d7ba1b84829801a57fd0c6918a
Robert Hubert <robertfhubert@gmail.com>
2019-07-03 16:56:16 -0700

    resolve error

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -8,15 +8,15 @@ class Api::ProjectsController < ApplicationController
         @project = Project.find(params[:id])
     end  
 
-    def create 
-        @project = Project.new(project_params)
-        @project.author_id = current_user.id
-        if @project.save 
-            render :show 
-        else   
-            render :json @project.errors.full_messages 
-        end  
-    end  
+    # def create 
+    #     @project = Project.new(project_params)
+    #     @project.author_id = current_user.id
+    #     if @project.save 
+    #         render :show 
+    #     else   
+    #         render :json @project.errors.full_messages 
+    #     end  
+    # end  
 
     def project_parmams 
         params.require(:project).permit(:description, :body, :materials)
