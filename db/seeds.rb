3fb8ae5ddd7edcc19d9c1e8b9cf4c561fdbc51bd
Robert Hubert <robertfhubert@gmail.com>
2019-07-04 16:32:39 -0700

    Add id in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,5 +12,5 @@ ActiveRecord::Base.transaction do
 
     Project.destroy_all
     makingLuresOneOOne = Project.create(description: "Making good all-purpose lures", body: "This is going to contain a lengthy post about making lures exactly
-    how you want to make them", materials: "twine, beads, fishingline, hooks", number_of_likes: 3, author_id: 12)
+    how you want to make them", materials: "twine, beads, fishingline, hooks", number_of_likes: 3, author_id: guestUser.id)
 end
